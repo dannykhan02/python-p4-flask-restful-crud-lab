fa7f768b69638982ea8956aee0c390a44c5931b0
dannykhan02 <dannykhan614@gmail.com>
2024-07-10 17:32:56 +0300

    create my file

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -47,9 +45,34 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+
+        if plant is None:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+        data = request.get_json()
+
+        for key, value in data.items():
+            setattr(plant, key, value)
+
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        if plant is None:
+            return make_response(jsonify({'message': 'Plant not found'}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)  
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(host='0.0.0.0', port=5555, debug=True)
